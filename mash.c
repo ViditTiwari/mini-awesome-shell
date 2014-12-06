6878e4585ba7bd186c6609ad77b22dc34dc05246
Vidit Tiwari <vidittiwari95@gmail.com>
2014-12-06 11:50:14 +0530

    signal-handler

--- mash.c
+++ mash.c
@@ -386,6 +386,44 @@ void init()
         }
 }
 
+void signalHandler_child(int p)
+{
+        pid_t pid;
+        int terminationStatus;
+        pid = waitpid(WAIT_ANY, &terminationStatus, WUNTRACED | WNOHANG);
+        if (pid > 0) {
+                t_job* job = getJob(pid, BY_PROCESS_ID);
+                if (job == NULL)
+                        return;
+                if (WIFEXITED(terminationStatus)) {
+                        if (job->status == BACKGROUND) {
+                                printf("\n[%d]+  Done\t   %s\n", job->id, job->name);
+                                jobsList = delJob(job);
+                        }
+                } else if (WIFSIGNALED(terminationStatus)) {
+                        printf("\n[%d]+  KILLED\t   %s\n", job->id, job->name);
+                        jobsList = delJob(job);
+                } else if (WIFSTOPPED(terminationStatus)) {
+                        if (job->status == BACKGROUND) {
+                                tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+                                changeJobStatus(pid, WAITING_INPUT);
+                                printf("\n[%d]+   suspended [wants input]\t   %s\n",
+                                       numActiveJobs, job->name);
+                        } else {
+                                tcsetpgrp(MSH_TERMINAL, job->pgid);
+                                changeJobStatus(pid, SUSPENDED);
+                                printf("\n[%d]+   stopped\t   %s\n", numActiveJobs, job->name);
+                        }
+                        return;
+                } else {
+                        if (job->status == BACKGROUND) {
+                                jobsList = delJob(job);
+                        }
+                }
+                tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+        }
+}
+
 int main(int argc, char **argv, char **envp)
 {
         init();
